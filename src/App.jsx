599826b55912a95ce00989ce7d964dcb97142fa1
nmcsakib <bksumon51@gmail.com>
2023-08-08 15:51:07 +0600

    test 01

--- src/App.jsx
+++ src/App.jsx
@@ -15,13 +15,17 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h2>Vite + React</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           the count {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
+          {
+            console.log('hello world')
+            
+          }
         </p>
       </div>
       <p className="read-the-docs">
