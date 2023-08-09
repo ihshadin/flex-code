fbae2f7243fd2ec33912c0d38e4f90576c630cf9
nmcsakib <bksumon51@gmail.com>
2023-08-09 22:38:11 +0600

    added router

---
+++ src/Home/OurPartners/OurParthers.jsx
@@ -0,0 +1,8 @@
+const OurPartners = () => {
+return (
+<div>
+    This is our partner.....
+</div>
+);
+};
+export default OurPartners;
--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -1,10 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
+import Home from "../Home/Home";
+import OurPartners from "../Home/OurPartners/OurParthers";
 
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: 'flex on construction'
+        element: <Home/>,
+        children: [
+            {
+                path: '/',
+                element: <OurPartners/>
+            }
+        ]
     }
 ])
 
