f6b14246e5b0a4cc48fbc3c716ab82a4d04768a3
nmcsakib <bksumon51@gmail.com>
2023-08-10 01:25:44 +0600

    added right router func

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -2,12 +2,20 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "../Home/Home";
 import OurPartners from "../Home/OurPartners/OurPartners";
 import UserCount from "../Home/UserCount/UserCount";
+import MainLayout from "../layouts/MainLayout";
 
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <Home/>,
+        element: <MainLayout/>,
+        children: [
+            {
+                path: '/',
+                element: <Home/>
+            }
+        ]
+        
     }
 ])
 
