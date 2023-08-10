02ebe18b9711f811e14553d68ba37df60bd0dfd4
OmarFaruq967 <rone967@gmail.com>
2023-08-10 18:59:03 +0600

    Slider added on home page

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -2,10 +2,12 @@ import { Outlet } from "react-router-dom";
 import OurPartners from "./OurPartners/OurPartners";
 import UserCount from "./UserCount/UserCount";
 import Faq from "./FAQ/Faq";
+import Slider from "./Slider/Slider";
 
 const Home = () => {
   return (
     <div className="px-5 md:px-10 container mx-auto">
+      <Slider/>
       <OurPartners />
       <UserCount />
       <Faq></Faq>
