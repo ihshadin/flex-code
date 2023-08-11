bca8373227d4434fe20dace6a5de850136bdec3f
Imam Hossain <shadinfr@yahoo.com>
2023-08-11 10:29:40 +0600

    Blog page design and explore design

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,13 +1,14 @@
-import { Outlet } from "react-router-dom";
 import OurPartners from "../OurPartners/OurPartners";
 import UserCount from "../UserCount/UserCount";
 import Faq from "../FAQ/Faq";
 import Slider from "../Slider/Slider";
+import ExploreUs from "../../../components/ExploreUs/ExploreUs";
 
 const Home = () => {
   return (
     <div>
       <Slider />
+      <ExploreUs />
       <OurPartners />
       <UserCount />
       <Faq></Faq>
