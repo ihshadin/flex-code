e7acdeb45fc8b7788dc0fe5892420ae7e891fd7e
studentAbuSayeed <studentabusayeed2@gmail.com>
2023-08-14 13:13:40 +0600

    db design

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -4,7 +4,6 @@ import Login from "../pages/Login/Login";
 import SignUp from "../pages/Signup/SignUp";
 import Blog from "../pages/Blog/Blog";
 import Home from "../pages/Home/Home/Home";
-import Dashboard from "../layouts/Dashboard";
 
 const router = createBrowserRouter([
   {
@@ -26,10 +25,6 @@ const router = createBrowserRouter([
       {
         path: "/blog",
         element: <Blog />,
-      },
-      {
-        path: "dashboard",
-        element: <Dashboard></Dashboard>
       }
     ],
   },
