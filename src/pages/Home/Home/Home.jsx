68a1188d726f73eee0e6a22869e401b290099f89
OmarFaruq967 <rone967@gmail.com>
2023-08-15 15:44:43 +0600

    Bannar Section added

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,15 +1,15 @@
 import OurPartners from "../OurPartners/OurPartners";
 import UserCount from "../UserCount/UserCount";
 import Faq from "../FAQ/Faq";
-import Slider from "../Slider/Slider";
 import ExploreUs from "../../../components/ExploreUs/ExploreUs";
 import Testimonials from "../../../components/Testimonials/Testimonials";
 import Editor from "../Editor/Editor";
+import Banner from "../Banner/Banner";
 
 const Home = () => {
   return (
     <div>
-      <Slider />
+      <Banner />
       <OurPartners />
       <ExploreUs />
       <UserCount />
