b5d1ba00992fd2e49111c186fa8e5e3f08ff3785
Imam Hossain <shadinfr@yahoo.com>
2023-08-15 18:54:09 +0600

    Our Partner section design

--- src/pages/Home/OurPartners/OurPartners.jsx
+++ src/pages/Home/OurPartners/OurPartners.jsx
@@ -1,11 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import Marquee from "react-fast-marquee";
-import Partner from "./Partner";
 import './Partner.css'
 
-
 const OurPartners = () => {
-  const [activeIndex, setActiveIndex] = useState(null);
   const partners = [
     { pic: "https://i.ibb.co/ScQyB8R/partner-8.png" },
     { pic: "https://i.ibb.co/K61rVW0/partner-7.png" },
@@ -16,33 +13,18 @@ const OurPartners = () => {
     { pic: "https://i.ibb.co/pzy6SkD/partner-6.png" },
     { pic: "https://i.ibb.co/gFfQwt3/partner-2.png" },
   ];
-
-  const handleMouseEnter = (index) => {
-    setActiveIndex(index);
-  };
-
-  const handleMouseLeave = () => {
-    setActiveIndex(null);
-  };
-
   return (
     <div className="flexcode-container">
       <Marquee
         pauseOnClick
         speed={200}
         gradient={true}
-        gradientColor={[23, 24, 27]}>
-        {" "}
-        {/* pauseOnHover */}
+        gradientColor={[23, 24, 27]}
+        children={null}
+        className="grayscale"
+      >
         {partners?.map((partner, index) => (
-          <div
-            key={index}
-            onMouseEnter={() => handleMouseEnter(index)}
-            onMouseLeave={handleMouseLeave}
-            className=" "
-          >
-            <Partner src={partner.pic} isActive={index === activeIndex} />
-          </div>
+          <img key={index} src={partner.pic} className="w-1/2" />
         ))}
       </Marquee>
     </div>
--- src/pages/Home/UserCount/UserCount.jsx
+++ src/pages/Home/UserCount/UserCount.jsx
@@ -4,31 +4,33 @@ const UserCount = () => {
   return (
     <>
       <section>
-        <div className=" rounded-xl border-[#00ffc3] border-2 drop-shadow-lg grid px-10 py-16 gap-10 lg:gap-0 lg:grid-cols-3 justify-center text-white montserrat">
-          <div className="flex flex-col gap-5 justify-center items-center">
-            <h2 className="text-7xl">
-              <CountUp start={0} end={1000} duration={4} /> &#43;
-            </h2>
-            <p className="text-lg tracking-wider font-thin">
-              Total users in FlexCode.
-            </p>
-          </div>
-          <div className="flex flex-col gap-5 justify-center items-center">
-            <h2 className="text-7xl">
-              <CountUp start={0} end={500} duration={3} /> &#43;
-            </h2>
-            <p className="text-lg tracking-wider">
-              Total problems in our site.
-            </p>
-          </div>
-          <div className="flex flex-col gap-5 justify-center items-center">
-            <h2 className="text-7xl">
-              {" "}
-              <CountUp start={0} end={451} duration={3} />{" "}
-            </h2>
-            <p className="text-lg tracking-wider">
-              Completed Problems in this month.
-            </p>
+        <div className="flexcode-container">
+          <div className="rounded-xl py-16 text-center border-[#00ffc356] border-2 drop-shadow-lg grid gap-10 lg:gap-0 lg:grid-cols-3 justify-center text-white  montserrat divide-x-2 divide-[#00ffc356]">
+            <div className="flex flex-col gap-5 justify-center items-center">
+              <h2 className="text-7xl font-bold">
+                <CountUp start={0} end={856} duration={4} />&#43;
+              </h2>
+              <p className="text-lg tracking-wider font-thin">
+                Total users in FlexCode.
+              </p>
+            </div>
+            <div className="flex flex-col gap-5 justify-center items-center">
+              <h2 className="text-7xl font-bold">
+                <CountUp start={0} end={518} duration={3} />&#43;
+              </h2>
+              <p className="text-lg tracking-wider">
+                Total problems in our site.
+              </p>
+            </div>
+            <div className="flex flex-col gap-5 justify-center items-center">
+              <h2 className="text-7xl font-bold">
+                {" "}
+                <CountUp start={0} end={451} duration={3} />{" "}
+              </h2>
+              <p className="text-lg tracking-wider">
+                Completed Problems in this month.
+              </p>
+            </div>
           </div>
         </div>
       </section>
