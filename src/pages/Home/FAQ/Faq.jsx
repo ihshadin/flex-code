4e5d1e98ee178b5c93be15fbe6512fe832e4fb6f
studentAbuSayeed <studentabusayeed2@gmail.com>
2023-08-16 13:43:30 +0600

    fag design change

--- src/pages/Home/FAQ/Faq.jsx
+++ src/pages/Home/FAQ/Faq.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Container from '../../Shared/Container/Container';
+import faqImg from '../../../assets/faq.jpg';
 
 const Faq = () => {
     return (
@@ -12,102 +13,107 @@ const Faq = () => {
 
                         </p>
                     </div>
-                    <div class="mx-auto mt-8 grid  divide-y divide-[#00ffc3]">
-                        <div class="py-5 px-2">
-                            <details class="group">
-                                <summary class="flex cursor-pointer list-none items-center justify-between font-medium">
-                                    <span>What is FlexCode?</span>
-                                    <span class="transition group-open:rotate-180">
-                                        <svg fill="none" height="24" shape-rendering="geometricPrecision"
-                                            stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                            stroke-width="1.5" viewBox="0 0 24 24" width="24">
-                                            <path d="M6 9l6 6 6-6"></path>
-                                        </svg>
-                                    </span>
-                                </summary>
-                                <p class="group-open:animate-fadeIn mt-3 text-white">FlexCode is a problem-solving platform that helps coding enthusiasts improve their skills through engaging challenges and interactive learning.
-                                </p>
-                            </details>
+                    <div className='flex justify-center items-center gap-10'>
+                        <div class="mx-auto w-[50%] mt-8 grid  divide-y divide-[#00ffc3]">
+                            <div class="py-5 px-2">
+                                <details class="group">
+                                    <summary class="flex cursor-pointer list-none items-center justify-between font-medium">
+                                        <span>What is FlexCode?</span>
+                                        <span class="transition group-open:rotate-180">
+                                            <svg fill="none" height="24" shape-rendering="geometricPrecision"
+                                                stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                                stroke-width="1.5" viewBox="0 0 24 24" width="24">
+                                                <path d="M6 9l6 6 6-6"></path>
+                                            </svg>
+                                        </span>
+                                    </summary>
+                                    <p class="group-open:animate-fadeIn mt-3 text-white">FlexCode is a problem-solving platform that helps coding enthusiasts improve their skills through engaging challenges and interactive learning.
+                                    </p>
+                                </details>
+                            </div>
+                            <div class="py-5 px-2">
+                                <details class="group">
+                                    <summary class="flex cursor-pointer list-none items-center justify-between font-medium">
+                                        <span>How do I join FlexCode?</span>
+                                        <span class="transition group-open:rotate-180">
+                                            <svg fill="none" height="24" shape-rendering="geometricPrecision"
+                                                stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                                stroke-width="1.5" viewBox="0 0 24 24" width="24">
+                                                <path d="M6 9l6 6 6-6"></path>
+                                            </svg>
+                                        </span>
+                                    </summary>
+                                    <p class="group-open:animate-fadeIn mt-3 text-white"> Joining FlexCode is easy! Simply sign up for an account, and you'll have access to a wide range of coding challenges and features.
+                                    </p>
+                                </details>
+                            </div>
+                            <div class="py-5 px-2">
+                                <details class="group">
+                                    <summary class="flex cursor-pointer list-none items-center justify-between font-medium">
+                                        <span>Is FlexCode suitable for beginners?</span>
+                                        <span class="transition group-open:rotate-180">
+                                            <svg fill="none" height="24" shape-rendering="geometricPrecision"
+                                                stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                                stroke-width="1.5" viewBox="0 0 24 24" width="24">
+                                                <path d="M6 9l6 6 6-6"></path>
+                                            </svg>
+                                        </span>
+                                    </summary>
+                                    <p class="group-open:animate-fadeIn mt-3 text-white">Yes, FlexCode is designed for all skill levels, including beginners. Our platform provides challenges and resources that cater to both newcomers and experienced coders.
+                                    </p>
+                                </details>
+                            </div>
+                            <div class="py-5 px-2">
+                                <details class="group">
+                                    <summary class="flex cursor-pointer list-none items-center justify-between font-medium">
+                                        <span>Are there any membership fees?</span>
+                                        <span class="transition group-open:rotate-180">
+                                            <svg fill="none" height="24" shape-rendering="geometricPrecision"
+                                                stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                                stroke-width="1.5" viewBox="0 0 24 24" width="24">
+                                                <path d="M6 9l6 6 6-6"></path>
+                                            </svg>
+                                        </span>
+                                    </summary>
+                                    <p class="group-open:animate-fadeIn mt-3 text-white">FlexCode offers a free basic membership with access to a selection of challenges. We also have a premium membership that provides access to our full range of challenges, advanced features, and personalized learning paths.
+                                    </p>
+                                </details>
+                            </div>
+                            <div class="py-5 px-2">
+                                <details class="group">
+                                    <summary class="flex cursor-pointer list-none items-center justify-between font-medium">
+                                        <span>How often are new challenges added?</span>
+                                        <span class="transition group-open:rotate-180">
+                                            <svg fill="none" height="24" shape-rendering="geometricPrecision"
+                                                stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                                stroke-width="1.5" viewBox="0 0 24 24" width="24">
+                                                <path d="M6 9l6 6 6-6"></path>
+                                            </svg>
+                                        </span>
+                                    </summary>
+                                    <p class="group-open:animate-fadeIn mt-3 text-white"> We regularly update FlexCode with new challenges to keep the content fresh and engaging. You can expect new challenges to be added on a weekly basis, offering you a continuous learning experience.
+                                    </p>
+                                </details>
+                            </div>
+                            <div class="py-5 px-2">
+                                <details class="group">
+                                    <summary class="flex cursor-pointer list-none items-center justify-between font-medium">
+                                        <span>Can I collaborate with other users on challenges?</span>
+                                        <span class="transition group-open:rotate-180">
+                                            <svg fill="none" height="24" shape-rendering="geometricPrecision"
+                                                stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                                stroke-width="1.5" viewBox="0 0 24 24" width="24">
+                                                <path d="M6 9l6 6 6-6"></path>
+                                            </svg>
+                                        </span>
+                                    </summary>
+                                    <p class="group-open:animate-fadeIn mt-3 text-white">Absolutely! FlexCode encourages collaboration among users. You can team up with others, tackle challenges together, and even join coding communities to learn and grow with fellow enthusiasts.
+                                    </p>
+                                </details>
+                            </div>
                         </div>
-                        <div class="py-5 px-2">
-                            <details class="group">
-                                <summary class="flex cursor-pointer list-none items-center justify-between font-medium">
-                                    <span>How do I join FlexCode?</span>
-                                    <span class="transition group-open:rotate-180">
-                                        <svg fill="none" height="24" shape-rendering="geometricPrecision"
-                                            stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                            stroke-width="1.5" viewBox="0 0 24 24" width="24">
-                                            <path d="M6 9l6 6 6-6"></path>
-                                        </svg>
-                                    </span>
-                                </summary>
-                                <p class="group-open:animate-fadeIn mt-3 text-white"> Joining FlexCode is easy! Simply sign up for an account, and you'll have access to a wide range of coding challenges and features.
-                                </p>
-                            </details>
-                        </div>
-                        <div class="py-5 px-2">
-                            <details class="group">
-                                <summary class="flex cursor-pointer list-none items-center justify-between font-medium">
-                                    <span>Is FlexCode suitable for beginners?</span>
-                                    <span class="transition group-open:rotate-180">
-                                        <svg fill="none" height="24" shape-rendering="geometricPrecision"
-                                            stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                            stroke-width="1.5" viewBox="0 0 24 24" width="24">
-                                            <path d="M6 9l6 6 6-6"></path>
-                                        </svg>
-                                    </span>
-                                </summary>
-                                <p class="group-open:animate-fadeIn mt-3 text-white">Yes, FlexCode is designed for all skill levels, including beginners. Our platform provides challenges and resources that cater to both newcomers and experienced coders.
-                                </p>
-                            </details>
-                        </div>
-                        <div class="py-5 px-2">
-                            <details class="group">
-                                <summary class="flex cursor-pointer list-none items-center justify-between font-medium">
-                                    <span>Are there any membership fees?</span>
-                                    <span class="transition group-open:rotate-180">
-                                        <svg fill="none" height="24" shape-rendering="geometricPrecision"
-                                            stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                            stroke-width="1.5" viewBox="0 0 24 24" width="24">
-                                            <path d="M6 9l6 6 6-6"></path>
-                                        </svg>
-                                    </span>
-                                </summary>
-                                <p class="group-open:animate-fadeIn mt-3 text-white">FlexCode offers a free basic membership with access to a selection of challenges. We also have a premium membership that provides access to our full range of challenges, advanced features, and personalized learning paths.
-                                </p>
-                            </details>
-                        </div>
-                        <div class="py-5 px-2">
-                            <details class="group">
-                                <summary class="flex cursor-pointer list-none items-center justify-between font-medium">
-                                    <span>How often are new challenges added?</span>
-                                    <span class="transition group-open:rotate-180">
-                                        <svg fill="none" height="24" shape-rendering="geometricPrecision"
-                                            stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                            stroke-width="1.5" viewBox="0 0 24 24" width="24">
-                                            <path d="M6 9l6 6 6-6"></path>
-                                        </svg>
-                                    </span>
-                                </summary>
-                                <p class="group-open:animate-fadeIn mt-3 text-white"> We regularly update FlexCode with new challenges to keep the content fresh and engaging. You can expect new challenges to be added on a weekly basis, offering you a continuous learning experience.
-                                </p>
-                            </details>
-                        </div>
-                        <div class="py-5 px-2">
-                            <details class="group">
-                                <summary class="flex cursor-pointer list-none items-center justify-between font-medium">
-                                    <span>Can I collaborate with other users on challenges?</span>
-                                    <span class="transition group-open:rotate-180">
-                                        <svg fill="none" height="24" shape-rendering="geometricPrecision"
-                                            stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                            stroke-width="1.5" viewBox="0 0 24 24" width="24">
-                                            <path d="M6 9l6 6 6-6"></path>
-                                        </svg>
-                                    </span>
-                                </summary>
-                                <p class="group-open:animate-fadeIn mt-3 text-white">Absolutely! FlexCode encourages collaboration among users. You can team up with others, tackle challenges together, and even join coding communities to learn and grow with fellow enthusiasts.
-                                </p>
-                            </details>
+                        <div className='w-[40%] mt-8'>
+                            <img className='rounded-lg' src={faqImg} alt="" />
                         </div>
                     </div>
                 </div>
