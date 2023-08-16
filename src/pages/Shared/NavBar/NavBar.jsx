894dd7a6b869f162f77727e59ca5767763829cbd
jahid <jahidmorol2@gmail.com>
2023-08-16 13:26:23 +0600

    some changes of navbar

--- src/pages/Shared/NavBar/NavBar.jsx
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -1,11 +1,14 @@
-import React from "react";
-import { useContext, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { AuthContext } from "../../../providers/AuthProvider";
+import Swal from "sweetalert2";
 
 const NavBar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [userClicked, setUserClicked] = useState(false);
+
   const location = useLocation();
+  const dropdownRef = useRef(null);
 
   const handleMenuToggle = () => {
     setIsMenuOpen(!isMenuOpen);
@@ -21,183 +24,261 @@ const NavBar = () => {
     return location.pathname === routePath;
   };
 
+  const handleSignOut = () => {
+    logOut().then(() => {
+      Swal.fire({
+        position: "bottom-start",
+        icon: "success",
+        title: "LogOut Success",
+        showConfirmButton: false,
+        timer: 1000,
+      });
+    });
+  };
+
+  const handleUserClick = () => {
+    setUserClicked(!userClicked);
+  };
+
+  useEffect(() => {
+    const handleClickOutsideDropdown = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setUserClicked(false);
+      }
+    };
+
+    document.addEventListener("click", handleClickOutsideDropdown);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutsideDropdown);
+    };
+  }, []);
+
   return (
-    <nav className=" fixed backdrop-blur-sm top-0 left-0 z-10 w-full">
-         <div className="bg-inherit py-4 lg:flex justify-between items-center px-4 lg:px-8">
-      <div className="flex items-center justify-between">
-        <Link to="/" className="text-white text-xl font-semibold">
-          <div className="flex items-center gap-1">
-            <img
-              className="w-[8rem] md:w-[10rem]"
-              src="/flex-codelogo.png"
-              alt="logo"
-            />
+    <nav className="fixed backdrop-blur-sm top-0 left-0 z-10 w-full">
+      <div className="bg-inherit py-4 lg:flex justify-between items-center px-4 lg:px-8">
+        <div className="flex items-center justify-between">
+          <Link to="/" className="text-white text-xl font-semibold">
+            <div className="flex items-center gap-1">
+              <img
+                className="w-[8rem] md:w-[10rem]"
+                src="/flex-codelogo.png"
+                alt="logo"
+              />
+            </div>
+          </Link>
+          <div className="ml-3 lg:hidden">
+            <button
+              onClick={handleMenuToggle}
+              className="text-white p-2 focus:outline-none transition-opacity duration-300 ease-in-out"
+              aria-controls="mobile-menu"
+              aria-expanded={isMenuOpen}
+              style={{ opacity: isMenuOpen ? 0.5 : 1 }}
+            >
+              {isMenuOpen ? (
+                <svg
+                  className="w-6 h-6"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  className="w-6 h-6"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16m-7 6h7"
+                  />
+                </svg>
+              )}
+            </button>
           </div>
-        </Link>
-        <div className="ml-3 lg:hidden">
-          <button
-            onClick={handleMenuToggle}
-            className="text-white p-2 focus:outline-none transition-opacity duration-300 ease-in-out"
-            aria-controls="mobile-menu"
-            aria-expanded={isMenuOpen}
-            style={{ opacity: isMenuOpen ? 0.5 : 1 }}
-          >
-            {isMenuOpen ? (
-              <svg
-                className="w-6 h-6"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            ) : (
-              <svg
-                className="w-6 h-6"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16m-7 6h7"
-                />
-              </svg>
-            )}
-          </button>
         </div>
-      </div>
-      <div className="flex items-center">
-        <div
-          className={`${
-            isMenuOpen ? "block" : "hidden"
-          } lg:flex lg:w-auto mt-4 lg:mt-0`}
-          id="mobile-menu"
-        >
-          <ul className="flex flex-col gap-3 md:gap-0  lg:text-left lg:flex-row lg:space-x-8 lg:items-center">
-            <li>
-              <Link
-                to="/"
-                onClick={closeMenu}
-                className={`text-white hover:text-gray-300 pb-1 hover:border-b-2 hover:border-[#00ffc3] transition-all duration-300 ${
-                  isActiveRoute("/")
-                    ? "font-bold border-b-2 pb-1 border-[#00ffc3]"
-                    : ""
-                }`}
-              >
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/blog"
-                onClick={closeMenu}
-                className={`text-white hover:text-gray-300 pb-1 hover:border-b-2 hover:border-[#00ffc3] transition-all duration-300 ${
-                  isActiveRoute("/blog")
-                    ? "font-bold md:border-b-2 pb-1 border-[#00ffc3]"
-                    : ""
-                }`}
-              >
-                Problems
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/admission"
-                onClick={closeMenu}
-                className={`text-white hover:text-gray-300 pb-1 hover:border-b-2 hover:border-[#00ffc3] transition-all duration-300 ${
-                  isActiveRoute("/admission")
-                    ? "font-bold md:border-b-2 pb-1 border-[#00ffc3]"
-                    : ""
-                }`}
-              >
-                About
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/dashboard"
-                onClick={closeMenu}
-                className={`text-white hover:text-gray-300 pb-1 hover:border-b-2 hover:border-[#00ffc3] transition-all duration-300 ${
-                  isActiveRoute("/myCollages")
-                    ? "font-bold md:border-b-2 pb-1 border-[#00ffc3]"
-                    : ""
-                }`}
-              >
-                Dashboard
-              </Link>
-            </li>
-            {user?.email ? (
-              <>
-                <Link to="/profile">
-                  <li className="flex items-center">
+        <div className="flex items-center">
+          <div
+            className={`${
+              isMenuOpen ? "block" : "hidden"
+            } lg:flex lg:w-auto mt-4 lg:mt-0`}
+            id="mobile-menu"
+          >
+            <ul className="flex flex-col gap-3 md:gap-0  lg:text-left lg:flex-row lg:space-x-8 lg:items-center">
+              <li>
+                <Link
+                  to="/"
+                  onClick={closeMenu}
+                  className={`text-white hover:text-gray-300 pb-1 hover:border-b-2 hover:border-[#00ffc3] transition-all duration-300 ${
+                    isActiveRoute("/")
+                      ? "font-bold border-b-2 pb-1 border-[#00ffc3]"
+                      : ""
+                  }`}
+                >
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/blog"
+                  onClick={closeMenu}
+                  className={`text-white hover:text-gray-300 pb-1 hover:border-b-2 hover:border-[#00ffc3] transition-all duration-300 ${
+                    isActiveRoute("/blog")
+                      ? "font-bold md:border-b-2 pb-1 border-[#00ffc3]"
+                      : ""
+                  }`}
+                >
+                  Problems
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/admission"
+                  onClick={closeMenu}
+                  className={`text-white hover:text-gray-300 pb-1 hover:border-b-2 hover:border-[#00ffc3] transition-all duration-300 ${
+                    isActiveRoute("/admission")
+                      ? "font-bold md:border-b-2 pb-1 border-[#00ffc3]"
+                      : ""
+                  }`}
+                >
+                  About
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/dashboard"
+                  onClick={closeMenu}
+                  className={`text-white hover:text-gray-300 pb-1 hover:border-b-2 hover:border-[#00ffc3] transition-all duration-300 ${
+                    isActiveRoute("/dashboard")
+                      ? "font-bold md:border-b-2 pb-1 border-[#00ffc3]"
+                      : ""
+                  }`}
+                >
+                  Dashboard
+                </Link>
+              </li>
+
+              {user?.email ? (
+                <li className="relative">
+                  <button
+                    onClick={handleUserClick}
+                    className={`flex items-center ${
+                      userClicked ? "text-gray-300" : "text-white"
+                    }`}
+                  >
+
                     {user?.photoURL && (
                       <img
-                        className="h-8 w-8 rounded-full mr-2"
+                        ref={dropdownRef} 
+                        // tabIndex={0}
+                        className={`h-9 w-9 rounded-full mr-2 ${
+                          userClicked
+                            ? "border-2 border-[#00ffc3]"
+                            : "border-2 border-transparent"
+                        } `}
                         title={user?.displayName}
                         src={user?.photoURL}
                         alt=""
                       />
                     )}
-                    <span className="text-white hover:text-gray-300">
-                      {user?.displayName}
-                    </span>
-                  </li>
-                </Link>
-                <li>
-                  <button
-                    onClick={logOut}
-                    className="text-white font-semibold hover:text-gray-300  pb-1 hover:border-b-2 hover:border-[#00ffc3] transition-all duration-300"
-                  >
-                    Log out
                   </button>
+                  {user?.email && (
+                    <div
+                      className={`absolute right-0 mt-2 z-10 transition-all duration-500 ${
+                        userClicked ? "block" : "hidden"
+                      }`}
+                    >
+
+
+                      <div className="bg-white dark:bg-dark-layer-1 w-auto py-2 rounded-md shadow-md">
+                        <div className="px-4 py-2">
+                          <div className="flex items-center">
+                            <img
+                              className="h-10 w-10 rounded-full object-cover mr-2"
+                              src={user.photoURL || "/default-avatar.png"}
+                              alt=""
+                            />
+                            <div>
+                              <p className="text-gray-800 dark:text-dark-label-1 font-medium">
+                                {user.displayName}
+                              </p>
+                              <p className="text-sm text-gray-500 dark:text-dark-label-3">
+                                {user.email}
+                              </p>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="border-t border-gray-300 dark:border-dark-divider-1">
+                          <Link
+                            to="/profile"
+                            className="block px-4 py-2 text-gray-800 dark:text-dark-label-1 hover:bg-gray-100 dark:hover:bg-dark-layer-2"
+                          >
+                            Profile
+                          </Link>
+                          <Link
+                            to="/dashboard"
+                            className="block px-4 py-2 text-gray-800 dark:text-dark-label-1 hover:bg-gray-100 dark:hover:bg-dark-layer-2"
+                          >
+                            Dashboard
+                          </Link>
+                          <button
+                            onClick={logOut}
+                            className="w-full text-left block px-4 py-2 text-gray-800 dark:text-dark-label-1 hover:bg-gray-100 dark:hover:bg-dark-layer-2"
+                          >
+                            Log Out
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  )}
                 </li>
-              </>
-            ) : (
-              <>
-                <li>
-                  <Link
-                    to="/login"
-                    onClick={closeMenu}
-                    className={`text-white font-semibold hover:text-gray-300  pb-1 hover:border-b-2 hover:border-[#00ffc3] transition-all duration-300 ${
-                      isActiveRoute("/login")
-                        ? "font-bold md:border-b-2 pb-1 border-[#00ffc3]"
-                        : ""
-                    }`}
-                  >
-                    Login
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="/signup"
-                    onClick={closeMenu}
-                    className={`text-white font-semibold hover:text-gray-300  pb-1 hover:border-b-2 hover:border-[#00ffc3] transition-all duration-300 ${
-                      isActiveRoute("/signup")
-                        ? "font-bold md:border-b-2 pb-1 border-[#00ffc3]"
-                        : ""
-                    }`}
-                  >
-                    Sign Up
-                  </Link>
-                </li>
-              </>
-            )}
-          </ul>
+              ) : (
+                <>
+                  <li>
+                    <Link
+                      to="/login"
+                      onClick={closeMenu}
+                      className={`text-white font-semibold hover:text-gray-300  pb-1 hover:border-b-2 hover:border-[#00ffc3] transition-all duration-300 ${
+                        isActiveRoute("/login")
+                          ? "font-bold md:border-b-2 pb-1 border-[#00ffc3]"
+                          : ""
+                      }`}
+                    >
+                      Login
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/signup"
+                      onClick={closeMenu}
+                      className={`text-white font-semibold hover:text-gray-300  pb-1 hover:border-b-2 hover:border-[#00ffc3] transition-all duration-300 ${
+                        isActiveRoute("/signup")
+                          ? "font-bold md:border-b-2 pb-1 border-[#00ffc3]"
+                          : ""
+                      }`}
+                    >
+                      Sign Up
+                    </Link>
+                  </li>
+                </>
+              )}
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
     </nav>
- 
   );
 };
 
