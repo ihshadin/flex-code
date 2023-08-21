02f5f1247457414fd68bfb91f74d63916a69d26d
jahid <jahidmorol2@gmail.com>
2023-08-22 01:11:46 +0600

    try to implant problem page

--- src/pages/Problems/ViewProblem.jsx
+++ src/pages/Problems/ViewProblem.jsx
@@ -4,15 +4,32 @@ import "codemirror/lib/codemirror.css";
 import "codemirror/mode/javascript/javascript";
 import "codemirror/theme/dracula.css";
 import Split from "react-split";
+import problem from '../../../public/problems.json'
 
 import './ViewProblem.css'
 
 const ViewProblem = () => {
 
-    const [code, setCode] = useState('console.log("Hello, world!");');
+    const [code, setCode] = useState(''); //console.log("Hello, world!");
     const [consoleOutput, setConsoleOutput] = useState([]);
-    const [selectedLanguage, setSelectedLanguage] = useState("javascript");
+    const [outputMessage, setOutputMessage] = useState(''); //new
 
+    //  new
+    const defaultCode = `
+    function ${problem.title}(${problem.parameterName[0]}){
+      // Your solution logic
+
+      return   ;
+    }
+  `;
+    
+    // new 
+    const handleCodeChange = (editor, data, value) => {
+        setCode(value);
+        setOutputMessage('');
+      };
+
+   
     const runCode = () => {
         try {
             // Clear console output
@@ -26,7 +43,10 @@ const ViewProblem = () => {
             };
 
             // Execute code
-            eval(code);
+           const userOutput = eval(code);
+            console.log(userOutput);
+
+
 
             // Restore console.log
             console.log = originalLog;
@@ -41,34 +61,32 @@ const ViewProblem = () => {
                 <div className="w-1/2">
                     <h2 className="text-2xl font-bold mb-2 primary-color">Two Sum</h2>
                     <p className="leading-loose">
-                        Given an array of integers <span className="px-2 border border-[#0fcda1] primary-color font-medium rounded-md">nums</span> and an integer <span className="px-2 border border-[#0fcda1] primary-color font-medium rounded-md">target</span>, return indices of the two numbers such that they add up to <span className="px-2 border border-[#0fcda1] primary-color font-medium rounded-md">target</span>.
+                        {/* Given an array of integers <span className="px-2 border border-[#0fcda1] primary-color font-medium rounded-md">nums</span> and an integer <span className="px-2 border border-[#0fcda1] primary-color font-medium rounded-md">target</span>, return indices of the two numbers such that they add up to <span className="px-2 border border-[#0fcda1] primary-color font-medium rounded-md">target</span>.
                         <br /><br />
                         You may assume that each input would have exactly one <b>solution</b>, and you may not use the same element twice.
                         <br /><br />
-                        You can return the answer in any order.
+                        You can return the answer in any order. */}
+                        {problem.problemsDetails}
                     </p>
                     <div className="flex flex-col gap-6 mt-10">
-                        <div>
-                            <h3 className="text-xl font-semibold">Example 1</h3>
-                            <div className="bg-secondary-color p-5 rounded-xl mt-2 flex flex-col gap-1 text-gray-400">
-                                <span><b className="text-white font-semibold">Input:</b> nums = [2,7,11,15], target = 9</span>
-                                <span><b className="text-white font-semibold">Output:</b> [0,1]</span>
+                            { 
+                            problem.examples.map(ex => (
+                            <div>
+                                <h3 className="text-xl font-semibold">{ex.example}</h3>
+                                <div className="bg-secondary-color p-5 rounded-xl mt-2 flex flex-col gap-1 text-gray-400">
+                                    <span><b className="text-white font-semibold">Input:</b> {ex.input} </span>
+                                    <span><b className="text-white font-semibold">Output:</b> {ex.output}</span>
+                                </div> 
                             </div>
-                        </div>
-                        <div>
+                            ))
+                            }
+                        {/* <div>
                             <h3 className="text-xl font-semibold">Example 2</h3>
                             <div className="bg-secondary-color p-5 rounded-xl mt-2 flex flex-col gap-1 text-gray-400">
                                 <span><b className="text-white font-semibold">Input:</b> nums = [2,7,11,15], target = 9</span>
                                 <span><b className="text-white font-semibold">Output:</b> [0,1]</span>
                             </div>
-                        </div>
-                        <div>
-                            <h3 className="text-xl font-semibold">Example 3</h3>
-                            <div className="bg-secondary-color p-5 rounded-xl mt-2 flex flex-col gap-1 text-gray-400">
-                                <span><b className="text-white font-semibold">Input:</b> nums = [2,7,11,15], target = 9</span>
-                                <span><b className="text-white font-semibold">Output:</b> [0,1]</span>
-                            </div>
-                        </div>
+                        </div> */}
                     </div>
                 </div>
                 <div className="w-1/2 flex flex-col">
@@ -100,7 +118,7 @@ const ViewProblem = () => {
                             <button onClick={runCode} className="flexcode-button text-xs py-2 px-3 ms-auto">Run Code</button>
                             <span className="block font-medium text-center">
                                 {consoleOutput == false ? "Console log" : consoleOutput.map((output, index) => (
-                                    <div key={index}>{output}</div>
+                                    <div key={index}><p></p>{output}</div>
                                 ))}
                             </span>
                             <div className="my-5">
