704994d30e68e6c17ea6e9d3fb9145e9b71e80f6
Imam Hossain <shadinfr@yahoo.com>
2023-08-26 12:21:10 +0600

    my submission fix

--- src/pages/Dashboard/Submissions/Submissions.jsx
+++ src/pages/Dashboard/Submissions/Submissions.jsx
@@ -1,43 +1,77 @@
-import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
-import { AuthContext } from '../../../providers/AuthProvider';
-import { FaUser } from "react-icons/fa";
+import React from "react";
+import {
+  BarChart,
+  Bar,
+  Brush,
+  ReferenceLine,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+import "./Submission.css";
 
 const Submissions = () => {
-  const { user } = useContext(AuthContext);
-  console.log(user);
+  const data = [
+    { name: "Jan", Submit: 456 },
+    { name: "Feb", Submit: 230 },
+    { name: "Mar", Submit: 345 },
+    { name: "Apr", Submit: 450 },
+    { name: "May", Submit: 321 },
+    { name: "Jun", Submit: 235 },
+    { name: "Jul", Submit: 267 },
+    { name: "Aug", Submit: 378 },
+    { name: "Sep", Submit: 210 },
+    { name: "Oct", Submit: 200 },
+    { name: "Nov", Submit: 150 },
+    { name: "Dec", Submit: 500 },
+  ];
   return (
-    <div className='bg-[#0E141E] min-h-screen'>
-      <div className='border-b-2 border-gray-500 py-5'>
-        <p className='text-slate-300 md:ml-36 mb-2'>All Submissions</p>
-        <h1 className='text-white md:ml-36 font-semibold'>Submissions</h1>
+    <div className="bg-secondary-color text-white md:ml-5 mt-4 rounded-lg pb-2">
+      <div className="flex justify-between text-base-300 pt-3 px-8">
+        <p className="pl-3"><span>0</span> submissions in the last year</p>
+        <div className="flex gap-x-2">
+          <p>Total active days: <span>0</span></p>
+          <p>Max streak: <span>0</span></p>
+        </div>
       </div>
-      <div className=" flex justify-center mt-10 bg-[#0E141E] text-white">
-        <table className="table table-striped table-bordered table-hover w-[60%] md:w-[80%] bg-secondary-color">
-          <thead>
-            <tr className='text-white border-gray-500 '>
-              <th>Label</th>
-              <th>Time Submitted</th>
-              <th>Question</th>
-              <th>Status</th>
-              <th>Language</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr className='border-0'>
-              <td>Medium</td>
-              <td>4 months, 2 weeks ago</td>
-              <td>Two Sum</td>
-              <td>
-                <Link className="text-success">
-                  <strong>Accepted</strong>
-                </Link>
-              </td>
-              <td>javascript</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      <ResponsiveContainer width="100%" height={250}>
+        <BarChart
+          data={data}
+          margin={{
+            top: 10,
+            right: 30,
+            left: 20,
+            bottom: 10,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" style={{ color: "black" }} />
+          <YAxis />
+          <Tooltip
+            contentStyle={{
+              backgroundColor: "#1e2d40b0",
+              border: "1px solid #fff",
+              borderRadius: "10px",
+            }}
+            labelStyle={{ color: "#ffffff", fontSize: "18px", fontWeight: "semibold" }}
+            itemStyle={{ color: "#ffffff" }}
+          />
+          <Legend verticalAlign="top" wrapperStyle={{ lineHeight: "10px" }} />
+          <ReferenceLine y={0} stroke="#0fcda1" />
+          <Brush dataKey="name" height={20} stroke="#fff" fill="#1e2d40" />
+          <Bar
+            dataKey="Submit"
+            fill="#0fcda1"
+            //   fill="#1e2d40" // Background color
+            className="hover:bg-black rounded-bar"
+            barSize={12}
+            radius={[3, 3, 0, 0]}
+          />
+        </BarChart>
+      </ResponsiveContainer>
     </div>
   );
 };
