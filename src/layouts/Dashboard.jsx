16eef595220003a0ead81cb5213bb5af9b40ffb3
Imam Hossain <shadinfr@yahoo.com>
2023-09-01 00:58:36 +0600

    problems design and signle page complete

--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -3,9 +3,15 @@ import { Link, Outlet } from "react-router-dom";
 import { AuthContext } from "../providers/AuthProvider";
 import NavBar from "../pages/Shared/NavBar/NavBar";
 import Footer from "../pages/Shared/Footer/Footer";
+import './Dashboard.css'
 
 const Dashboard = () => {
   const { user } = useContext(AuthContext);
+
+  const isActiveRoute = (routePath) => {
+    return location.pathname === routePath;
+  };
+
   return (
     <section>
       <NavBar />
@@ -14,7 +20,7 @@ const Dashboard = () => {
 
         <div className="drawer-content flex flex-col items-center">
           {/* Page content here */}
-          <div className="w-full ">
+          <div className="w-full">
             <Outlet />
             {/* <Footer /> */}
           </div>
@@ -28,38 +34,29 @@ const Dashboard = () => {
         <div className="drawer-side">
           <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
 
-          <div className="bg-gray-800 menu p-4 w-80 text-white py-5 rounded-lg h-screen">
+          <div className="flexcode-banner-bg p-4 w-80 text-white py-5 rounded-lg h-screen">
             <div className="pt-8 pb-6 mb-4 border-b-2 border-[#0fcda185]">
               <div className="flex justify-center items-center mb-4">
                 <img
-                  alt="profile-user"
-                  width="100px"
-                  height="100px"
                   src={user?.photoURL}
-                  style={{
-                    cursor: "pointer",
-                    borderRadius: "50%",
-                    border: "2px solid #0fcda185",
-                    padding: "5px",
-                  }}
+                  className="w-[100px] h-[100px] object-cover rounded-full border border-[#0fcda185] p-1 cursor-pointer"
+                  alt="profile-user"
                 />
               </div>
               <div className="text-center">
-                <h2 className="text-2xl text-white font-semibold mb-1">
+                <h2 className="text-2xl text-white font-semibold">
                   {user?.displayName}
                 </h2>
-                <h5 className="text-green-500">{user?.email}</h5>
+                <p className="text-slate-400">{user?.email}</p>
               </div>
             </div>
             <ul>
               <li>
                 <Link
                   to="/dashboard/main"
-                  className="flex items-center px-4 py-2 text-white hover:text-white hover:bg-[#0fcda1] hover:bg-opacity-40 mt-2"
+                  className="flex items-center gap-2 px-4 py-2 text-white hover:text-white hover:bg-[#0fcda1] hover:bg-opacity-40 mt-2"
                 >
-                  <span className="mr-2  flex">
-                    <span className="inline-block h-2 w-2 rounded-full bg-teal-300"></span>
-                  </span>
+                  <span className="inline-block h-2 w-2 rounded-full bg-teal-300"></span>
                   <span>Dashboard Home</span>
                 </Link>
               </li>
--- src/pages/AdminDashbord/AdminMainContent/OverViewAd/OverViewAd.jsx
+++ src/pages/AdminDashbord/AdminMainContent/OverViewAd/OverViewAd.jsx
@@ -1,156 +1,16 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-import { FaUserAlt, FaArrowUp, FaHighlighter } from "react-icons/fa";
-import { FaUsers, FaBug } from "react-icons/fa6";
+import UserCard from "./UserCard";
+import ProblemsCard from "./ProblemsCard";
+import BlogCard from "./BlogCard";
+import PreUserCard from "./PreUserCard";
 
 const OverViewAd = () => {
-  const [users, setUsers] = useState([]);
-  const [problems, setProblems] = useState([]);
-  const [payment, setPayment] = useState([]);
-  const [blogs, setBlogs] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get("http://localhost:5000/student/all")
-      .then((data) => setUsers(data.data));
-  }, []);
-
-  useEffect(() => {
-    axios
-      .get("http://localhost:5000/problem")
-      .then((data) => setProblems(data.data));
-  }, []);
-
-  useEffect(() => {
-    axios
-      .get("http://localhost:5000/payment")
-      .then((data) => setPayment(data.data));
-  }, []);
-
-  useEffect(() => {
-    axios
-      .get("http://localhost:5000/blog/all")
-      .then((data) => setBlogs(data.data.result));
-  }, []);
-
-  console.log(payment);
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-4 ">
-      {/* total users  */}
-      <div className="rounded-lg border-2 border-gray-400 transition-all group hover:border-[#0fcda1] bg-[#1e2d40] py-4 px-8 shadow-lg">
-        <FaUserAlt size={35} className="text-[#0fcda1]" />
-        <div className="mt-4 flex items-end justify-between">
-          <div>
-            <h4 className="text-2xl font-bold text-white group-hover:text-gray-300 flex items-center">
-              {users.length}
-              <span className="group-hover:text-[#0fcda1] text-3xl font-bold">
-                +
-              </span>
-            </h4>
-            <span className="text-sm font-medium text-slate-200 mr-5 mt-2">
-              Total Users
-            </span>
-          </div>
-
-          <span className="flex items-center gap-1 text-sm font-medium text-meta-3 text-slate-200">
-            0.43%
-            <FaArrowUp></FaArrowUp>
-          </span>
-        </div>
-      </div>
-
-      {/* Total Problem  */}
-      <div className="rounded-lg border-2 border-gray-400 transition-all group hover:border-[#0fcda1] bg-[#1e2d40] py-4 px-8 shadow-lg">
-        <FaBug size={35} className="text-[#0fcda1]" />
-        <div className="mt-4 flex items-end justify-between">
-          <div>
-            <h4 className="text-2xl font-bold text-white group-hover:text-gray-300 flex items-center">
-              {problems.length}
-              <span className="group-hover:text-[#0fcda1] text-3xl font-bold">
-                +
-              </span>
-            </h4>
-            <span className="text-sm font-medium text-slate-200 mr-5 mt-2">
-              Total Problem
-            </span>
-          </div>
-
-          <span className="flex items-center gap-1 text-sm font-medium text-meta-3 text-slate-200">
-            0.43%
-            <FaArrowUp></FaArrowUp>
-          </span>
-        </div>
-      </div>
-
-      {/* Total Blogs  */}
-
-      <div className="rounded-lg border-2 border-gray-400 transition-all group hover:border-[#0fcda1] bg-[#1e2d40] py-4 px-8 shadow-lg">
-        <FaHighlighter size={35} className="text-[#0fcda1]" />
-        <div className="mt-4 flex items-end justify-between">
-          <div>
-            <h4 className="text-2xl font-bold text-white group-hover:text-gray-300 flex items-center">
-              {blogs.length}
-              <span className="group-hover:text-[#0fcda1] text-3xl font-bold">
-                +
-              </span>
-            </h4>
-            <span className="text-sm font-medium text-slate-200 mr-5 mt-2">
-              Total blog
-            </span>
-          </div>
-
-          <span className="flex items-center gap-1 text-sm font-medium text-meta-3 text-slate-200">
-            0.43%
-            <FaArrowUp></FaArrowUp>
-          </span>
-        </div>
-      </div>
-
-      {/* Premium users  */}
-
-      <div className="rounded-lg border-2 border-gray-400 transition-all group hover:border-[#0fcda1] bg-[#1e2d40] py-4 px-8 shadow-lg">
-        <FaUsers size={35} className="text-[#0fcda1]" />
-        <div className="mt-4 flex items-end justify-between">
-          <div>
-            <h4 className="text-2xl font-bold text-white group-hover:text-gray-300 flex items-center">
-              {payment.length}
-              <span className="group-hover:text-[#0fcda1] text-3xl font-bold">
-                +
-              </span>
-            </h4>
-            <span className="text-sm font-medium text-slate-200 mr-5 mt-2">
-              Total Premium User
-            </span>
-          </div>
-
-          <span className="flex items-center gap-1 text-sm font-medium text-meta-3 text-slate-200">
-            0.43%
-            <FaArrowUp></FaArrowUp>
-          </span>
-        </div>
-      </div>
-      {/* <div className="rounded-lg border border-stroke bg-[#1e2d40] py-4 px-8 shadow-lg">
-        <div className="flex">
-          <FaUsers className="w-12 h-12 text-[#0fcda1]"></FaUsers>
-        </div>
-
-        <div className="mt-4 flex items-end justify-between">
-          <div>
-            <h4 className="text-2xl font-bold text-white dark:text-white mr-14">
-              $3.456K
-            </h4>
-            <span className="text-sm font-medium text-slate-200 mr-5 mt-2">
-              Total Premium Solve
-            </span>
-          </div>
-
-          <span className="flex items-center gap-1 text-sm font-medium text-meta-3 text-slate-200">
-            0.43%
-            <FaArrowUp></FaArrowUp>
-          </span>
-        </div>
-      </div> */}
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+      <UserCard />
+      <ProblemsCard />
+      <BlogCard />
+      <PreUserCard />
     </div>
   );
 };
