095401e0cd97372bc2806fb2f0d098622b888377
mehedihasan8 <mehedihasanfoysal22@gmail.com>
2023-09-08 11:50:27 +0600

    navbar responsive done

--- src/Hooks/useFlexUser.jsx
+++ src/Hooks/useFlexUser.jsx
@@ -1,21 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import useAuth from './useAuth';
-import useAxiosNormal from './useAxiosNormal';
+import React, { useEffect, useState } from "react";
+import useAuth from "./useAuth";
+import useAxiosNormal from "./useAxiosNormal";
 
 const useFlexUser = () => {
-    const { user, setLoading } = useAuth();
-    const [flexUser, setFlexUser] = useState(null);
-    const [axiosNormal] = useAxiosNormal();
+  const { user, setLoading } = useAuth();
+  const [flexUser, setFlexUser] = useState(null);
+  const [axiosNormal] = useAxiosNormal();
 
-    useEffect(() => {
-        axiosNormal.get(`/users?email=${user?.email}`)
-            .then(data => {
-                setFlexUser(data);
-                setLoading(false)
-            })
-    }, [user?.email])
+  useEffect(() => {
+    axiosNormal.get(`/users?email=${user?.email}`).then((data) => {
+      setFlexUser(data);
+      setLoading(false);
+    });
+  }, [user?.email]);
 
-    return [flexUser];
+  return [flexUser];
 };
 
 export default useFlexUser;
--- src/Hooks/useTopperData.jsx
+++ src/Hooks/useTopperData.jsx
@@ -1,20 +1,19 @@
-import React, { useEffect, useState } from 'react';
-import useAxiosNormal from './useAxiosNormal';
-import useAuth from './useAuth';
+import React, { useEffect, useState } from "react";
+import useAxiosNormal from "./useAxiosNormal";
+import useAuth from "./useAuth";
 
 const useTopperData = () => {
-    const { user } = useAuth();
-    const [topperData, setTopperData] = useState([]);
-    const [axiosNormal] = useAxiosNormal();
+  const { user } = useAuth();
+  const [topperData, setTopperData] = useState([]);
+  const [axiosNormal] = useAxiosNormal();
 
-    useEffect(() => {
-        axiosNormal.get('/solvedProblems/topperdata')
-            .then(data => {
-                setTopperData(data);
-            })
-    }, [user?.email])
+  useEffect(() => {
+    axiosNormal.get("/solvedProblems/topperdata").then((data) => {
+      setTopperData(data);
+    });
+  }, [user?.email]);
 
-    return [topperData];
+  return [topperData];
 };
 
 export default useTopperData;
